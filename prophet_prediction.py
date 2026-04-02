08423dcbde60871f23bde3ae8d150445d80e2624
IzzyBedic <izzy@bedichek.org>
2026-04-02 14:49:34 -0700

    merging still

--- prophet_prediction.py
+++ prophet_prediction.py
@@ -1,6 +1,5 @@
 # https://pypi.org/project/prophet/
 import pandas as pd
-
 from prophet import Prophet
 
 ### Goal: "Days until stock-out" prediction per item. ###
@@ -9,10 +8,18 @@ from prophet import Prophet
 weather = pd.read_csv("data/palo_alto_weather.csv")
 inventory = pd.read_csv("data/inventory.csv")
 
-# making sure both date columns are the same type
-inventory["date"] = pd.to_datetime(inventory["record_date"])
-weather["date"] = pd.to_datetime(weather["time"])
+# making sure both date columns are date type and named appropraitely
+inventory["record_date"] = pd.to_datetime(inventory["record_date"])
+weather["time"] = pd.to_datetime(weather["time"])
+inventory.rename(columns={"record_date": "DS"}, inplace=True)
+weather.rename(columns={"time": "DS"}, inplace=True)
 
-df = inventory.merge(weather, on="date", how='left')
+df = inventory.merge(weather, on="DS", how="left")
+
+df.rename(columns={"quantity": "Y"}, inplace=True)
 
 print(df.head())
+
+# ref https://facebook.github.io/prophet/docs/seasonality,_holiday_effects,_and_regressors.html#coefficients-of-additional-regressors
+# ref https://nbviewer.org/github/nicolasfauchereau/Auckland_Cycling/blob/master/notebooks/Auckland_cycling_and_weather.ipynb
+#m = Prophet(df, daily_seasonality= True)
